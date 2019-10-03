3d0a7d0c01b8a34a071d58d9e040e0a1b98c4812
Lưu Hưng <hunglv@fimo.edu.vn>
2019-10-03 16:06:09 +0700

    bug fix

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,2 @@
-from .vgg import *
-from .lenet import *
-from .resnet import *
-from .resnext import *
-from .densenet import *
-from .googlenet import *
-from .mobilenet import *
-# from .densenet_efficient_multi_gpu import DenseNet190
-from .densenet3 import DenseNet190
 from .hungnet import *
+from .zhangnet import *
